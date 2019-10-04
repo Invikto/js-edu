7d2b3242fac32dd7c51b2d3ec08b3fcfc7cf9cd0
Arthur Kuznetsov <artorius01@gmail.com>
2019-10-04 23:57:53 +0300

    feat: implement 'getTimeForEducation' function

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+  focus = 'family',
+  knowsProgramming = true,
+  config = {
+    family: 4
+  }
+) {
+  const loading = knowsProgramming ? 800 : 1300;
+  const result = Math.ceil(loading / config[focus]);
+
+  return result;
+}
